1914d3aae16df00b9ab23d6010df7103757dd531
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-05-08 11:16:09 +0530

    refactor: added absolute imports

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from src import env_config
+from src.config import env_config
 
 if __name__ == "__main__":
     uvicorn.run("src.app:app", host=env_config.HOST, port=env_config.PORT, reload=True)
