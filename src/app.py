7bfaa733e4a17b101ba09adfeb7a3bc425a25210
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-05-08 10:41:20 +0530

    refactor: return centralized ApiResponse and uses  __init__ imports

--- src/app.py
+++ src/app.py
@@ -1,17 +1,20 @@
 from fastapi import FastAPI
-
+from src import healthCheckRouter
+from src import ApiResponse
 
 app = FastAPI()
 
-# IMPORT ROUTERS
-from src.routes.healthCheck import router as healthCheck
-
 # INCLUDE ROUTERS
 api_prefix="/api"
-app.include_router(healthCheck, prefix=api_prefix)
+app.include_router(healthCheckRouter, prefix=api_prefix)
 
 
 # ROOT ENDPOINT
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the Offline KYC Service!"}
+    response = ApiResponse(
+        success=True,
+        status_code=200,
+        message="Welcome to the Offline KYC Service!"
+    )
+    return response.model_dump()
