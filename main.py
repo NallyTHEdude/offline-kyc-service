0a7422c9639ae47fbf9ffbf645c21604e63bf380
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-05-08 11:44:40 +0530

    feat: server start logging

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
-from src.config import env_config
+from src.config import env_config, logger
 
 if __name__ == "__main__":
+    logger.info(f"--------Starting server on {env_config.HOST}:{env_config.PORT}--------")
     uvicorn.run("src.app:app", host=env_config.HOST, port=env_config.PORT, reload=True)
