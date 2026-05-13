a30dd645480710c31e7dbe2812243dc08cb9f586
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-05-13 19:46:00 +0530

    chore: added _temp dir's path

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,5 +1,5 @@
 from src.config.env import env_config
 from src.config.logger import logger
-from src.config.paths import ROOT_DIR
+from src.config.paths import ROOT_DIR, TEMP_DIR
 
-__all__ = ["env_config", "logger", "ROOT_DIR", "IMAGE_DIR"]
+__all__ = ["env_config", "logger", "ROOT_DIR", "TEMP_DIR"]
