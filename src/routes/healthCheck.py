387d01c8c29a2a55266bd57ce00b2baa78eb2aa7
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-05-13 13:25:52 +0530

    refactor: using standard ApiResponse

--- src/routes/healthCheck.py
+++ src/routes/healthCheck.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter 
+from src.utils import ApiResponse
 
 router = APIRouter()
 
 @router.get("/health")
 def health_check():
-    return {"status": "200", "message": "Service is healthy"}
+    return ApiResponse(
+        success=True,
+        status_code=200,
+        message="Service is healthy"
+    )
