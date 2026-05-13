50c0d3242a6714c674775db9022c7554461e52be
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-05-13 13:12:49 +0530

    fix: updated _extract_data for better data handling

--- src/core/upload_file_service.py
+++ src/core/upload_file_service.py
@@ -2,6 +2,7 @@ import cv2
 import gzip
 from pyzbar.pyzbar import decode
 from src.config import logger
+import re
 
 
 def get_qr_data(image_path):
@@ -14,7 +15,7 @@ def get_qr_data(image_path):
         else:
             raise ValueError("No QR code detected in the image.")
     except Exception as e:
-        logger.error("Error in get_qr_data: %s", str(e))
+        logger.error("Error in get_qr_data: {}", str(e))
         return None
     
 # TODO: IMPLEMEMT OCR to extract text data from image if QR code is not detected
@@ -23,10 +24,10 @@ def get_image_data(image_path):
 
 # ---------- Helper Functions -----------
 def _preprocess_image(image_path):
-    logger.info("Reading image from path: %s", image_path)
+    logger.info("Reading image from path: {}", image_path)
     image = cv2.imread(image_path)
     if image is None:
-        logger.error("Could not read image at path: %s", image_path)
+        logger.error("Could not read image at path: {}", image_path)
         raise ValueError("Invalid image path.")
     logger.info("Image read successfully.")
     processed = cv2.resize(image, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
@@ -40,31 +41,28 @@ def detect_and_decode_qr(image):
         return None
     for obj in decoded_objects:
         data = obj.data.decode("utf-8")
-        logger.info("Decoded: %s", data)
         return data
 
-
-#TODO: handle data properly instead of hardcoding indices
 def _extract_data(decoded_qr_str):
     num = int(decoded_qr_str)
-    byte_data = num.to_bytes((num.bit_length() + 7) // 8, 'big')
+    byte_data = num.to_bytes((num.bit_length() + 7) // 8,'big')
     decompressed = gzip.decompress(byte_data)
-
-    name, dob, gender, phone_no = None, None, None, None
-
     data_parts = decompressed.split(b'\xff')
-    for i, part in enumerate(data_parts):
-        if i==3:
-            name = part.decode('utf-8')
-        elif i==4:
-            dob = part.decode('utf-8')
-        elif i==5:
-            gender = part.decode('utf-8')
-        elif i==17:
-            phone_no = part.decode('utf-8')
+    logger.info("Data parts count: {}", len(data_parts))
+    decoded_parts = []
+    for part in data_parts:
+        try:
+            decoded_parts.append(part.decode("utf-8"))
+        except Exception:
+            decoded_parts.append("")
+    phone_no = None
+    for value in decoded_parts:
+        if re.fullmatch(r"XXXXXX\d{4}", value):
+            phone_no = value
+            break
     return {
-        "name": name,
-        "dob": dob,
-        "gender": gender,
+        "name": decoded_parts[3],
+        "dob": decoded_parts[4],
+        "gender": decoded_parts[5],
         "phone_no": phone_no
     }
