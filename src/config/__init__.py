a376c35bd4907458bf551e36838ae22f6e324fbf
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-05-13 13:15:11 +0530

    chore: initialized paths for easier dir access

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,4 +1,5 @@
 from src.config.env import env_config
 from src.config.logger import logger
+from src.config.paths import ROOT_DIR
 
-__all__ = ["env_config", "logger"]
+__all__ = ["env_config", "logger", "ROOT_DIR", "IMAGE_DIR"]
