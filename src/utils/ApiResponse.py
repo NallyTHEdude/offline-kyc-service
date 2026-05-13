5bfef97af9372b337cec15850de8d744d399730b
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-05-13 18:09:51 +0530

    refactor: renamed utils to schemas

--- src/utils/ApiResponse.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional, Any
-from pydantic import BaseModel
-
-class ApiResponse(BaseModel):
-    success: bool = True
-    status_code: int
-    message: str
-    data: Optional[Any] = None
--- src/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.utils.ApiError import ApiError
-from src.utils.ApiResponse import ApiResponse
-
-__all__ = ["ApiError", "ApiResponse"]
