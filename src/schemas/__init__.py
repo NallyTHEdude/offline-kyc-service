82a351aba9283320935609f14b9df0f5137c7bd8
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-05-13 18:08:57 +0530

    chore: added adhaarData schema

---
+++ src/schemas/__init__.py
@@ -0,0 +1,5 @@
+from src.schemas.ApiError import ApiError
+from src.schemas.ApiResponse import ApiResponse
+from src.schemas.AdhaarData import AdhaarData
+
+__all__ = ["ApiError", "ApiResponse", "AdhaarData"]
