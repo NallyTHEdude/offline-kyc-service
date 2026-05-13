a6138326e656a78cf16074e7fbbee3f8f9212ca0
NallyTHEdude <rifaqatnawaz25@gmail.com>
2026-05-13 19:33:21 +0530

    refactor: explicitely exporting ErrorDetails

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,5 +1,6 @@
 from src.schemas.ApiError import ApiError
 from src.schemas.ApiResponse import ApiResponse
 from src.schemas.AdhaarData import AdhaarData
+from src.schemas.ApiError import ErrorDetails
 
-__all__ = ["ApiError", "ApiResponse", "AdhaarData"]
+__all__ = ["ApiError", "ApiResponse", "AdhaarData", "ErrorDetails"]
